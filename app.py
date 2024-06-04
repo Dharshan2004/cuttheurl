0467f1be11a6b2ccabed05c85d5ada942a7c344f
Kannan Priyadharshan <kpd2204@gmail.com>
2024-06-04 19:06:56 +0800

    Added new routes and initialised database

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, render_template, redirect # type: ignore
+from flask import Flask, request, render_template, redirect, url_for, session
 
 # initialize the Flask app
 app = Flask(__name__)
+app.secret_key = 'secret'
 
+# GET method to render the index.html
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('index.html')
+    shortend_url = session['url']
+    session.clear() # clear the session
+    return render_template('index.html', url=shortend_url) # render the index.html with the shortened URL(if any)
+
+# POST method to shorten the URL
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    url = request.form['url']
+    session['url'] = url
+    return redirect(url_for('main'))
 
 @app.route('/<id>', methods=['GET'])
 def redirect_url(id):
     return "Hello world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
