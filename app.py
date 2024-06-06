140de059fbb9a0323d5f9fe3db91a79dbb381015
Kannan Priyadharshan <kpd2204@gmail.com>
2024-06-06 12:13:49 +0800

    Added database functions and redirecting functionality

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, render_template, redirect, url_for, session
+from db_functions import insert_url, get_url
 
 # initialize the Flask app
 app = Flask(__name__)
 app.secret_key = 'secret'
+session = {"url": ""}
 
 # GET method to render the index.html
 @app.route('/', methods=['GET'])
 def main():
     shortend_url = session['url']
     session.clear() # clear the session
+    print(shortend_url)
     return render_template('index.html', url=shortend_url) # render the index.html with the shortened URL(if any)
 
 # POST method to shorten the URL
@@ -20,7 +23,10 @@ def shorten():
 
 @app.route('/<id>', methods=['GET'])
 def redirect_url(id):
-    return "Hello world"
+    print(id)
+    url = get_url(id)
+    print(url)
+    return redirect(url)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db_functions.py
+++ db_functions.py
@@ -1,4 +1,46 @@
 import sqlite3
+import random
+import string
 
+def generate_id():
+    id = ''.join([random.choice(string.ascii_letters
+            + string.digits) for n in range(10)])
+    return id
+ 
 def create_database():
-    pass
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE "url_directory" (
+	"id"	TEXT UNIQUE,
+	"actual_url"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);''')
+    con.commit()
+    con.close()
+
+def insert_url(id, actual_url):
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    id = generate_id()
+    if actual_url.startswith("http://") or actual_url.startswith("https://"):
+        actual_url = actual_url
+    else:
+        actual_url = "http://" + actual_url
+    while True:
+        try:
+            cur.execute("INSERT INTO url_directory (id, actual_url) VALUES (?, ?)", (id, actual_url))
+            con.commit()
+            break
+        except sqlite3.IntegrityError:
+            id = generate_id()
+    con.commit()
+    con.close()
+
+def get_url(id):
+    con = sqlite3.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT actual_url FROM url_directory WHERE id = ?", (id,))
+    con.commit()
+    url = cur.fetchone()[0]
+    con.close()
+    return url
