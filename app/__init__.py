7bc76b394aa16d23dfc3f4560c778b22998cf054
Kannan Priyadharshan <kpd2204@gmail.com>
2024-06-19 22:46:44 +0800

    [CTU-1] Restructured the routes to separate modular files

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, session
+from .main import main as main_blueprint
+
+session = {"url": ""} # session to store the shortened URL
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret'
+    app.register_error_handler(404, page_not_found)
+
+    # REGISTER BLUEPRINTS
+    app.register_blueprint(main_blueprint)
+
+    return app
---
+++ app/main/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect, url_for, session, request
+from . import main
+from app.db_functions import insert_url, get_url
+
+@main.route('/', methods=['GET'])
+def index():
+    shortend_url = session['url']
+    session['url'] = ""  # clear the session
+    print(shortend_url)
+    return render_template('index.html', url=shortend_url) # render the index.html with the shortened URL(if any)
+
+@main.route('/shorten', methods=['POST'])
+def shorten():
+    url = request.form['url']
+    id = insert_url(url)
+    session['url'] = f"http://localhost:5000/{id}"
+    return redirect(url_for('main.index'))
+
+@main.route('/<id>', methods=['GET'])
+def redirect_url(id):
+    url = get_url(id)
+    print(url)
+    if url is None:
+        print("URL not found")
+        return(redirect(url_for('page_not_found')))
+    else:
+        print("there is a url")
+        return(redirect(url))
+
+
